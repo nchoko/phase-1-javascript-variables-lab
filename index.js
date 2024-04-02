1bf6a052760e56d04cf5b56ae388d0300b6f7d5c
nchoko <orbaborthug@gmail.com>
2024-04-02 15:21:19 +0300

    initial comment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const companyName ='Scuber';
+function name(companyName) {
+    if (companyName === scuber);
+    console.log(scuber)
+    return;
+}
+name;
+
+let mostProfitableNeighborhood = 'Chelsea';
+function name1(mostProfitableNeighborhood) {
+    if (mostProfitableNeighborhood === Chelsea);
+    console.log(chelsea);
+    return;
+}
+name1;
+
+let companyCeo = "Susan Smith";
+function name(companyCeo) {
+    if (companyCeo === SusanSmith);
+    console.log(SusanSmith);
+    return;
+}
+name;
